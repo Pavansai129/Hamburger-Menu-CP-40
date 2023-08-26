0a967f8da4986a0ff8a48155ec9f0d76386595c1
pavansai <pavansai463@gmail.com>
2023-08-26 20:54:27 +0000

    all testcases passed

---
+++ src/components/About/index.js
@@ -0,0 +1,15 @@
+import Header from '../Header'
+// Write your code here
+
+const About = () => (
+  <>
+    <Header />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-lg-img.png"
+      alt="about"
+    />
+    <h1>About</h1>
+  </>
+)
+
+export default About
---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import {AiFillHome} from 'react-icons/ai'
+import Header from '../Header'
+
+const Home = () => (
+  <>
+    <Header />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-lg-img.png"
+      alt="home"
+    />
+    <h1>Home</h1>
+  </>
+)
+
+export default Home
